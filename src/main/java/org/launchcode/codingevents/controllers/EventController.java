4bfe3b8a0c1280625d2531f8be7e77e1cdc33baa
Lori <148814155+Lbeckap@users.noreply.github.com>
2024-10-14 01:09:07 -0400

    updated

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -8,17 +8,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping("events")
 public class EventController {
 
-    private static List<String> events = new ArrayList<>();
+    private static Map<String, String> events = new HashMap<>();
+    private static String url;
 
     @GetMapping
    public String displayAllEvents (Model model) {
        model.addAttribute("events" , events);
+       model.addAttribute("imgUrl", url);
        return "events/index";
    }
 
@@ -28,8 +32,10 @@ public class EventController {
    }
 
    @PostMapping("create") // it's okay to have the same path as renderCreateEventForm because they handle different types of requests. If they both handled the same type of request they would not be able to have the same path
-   public String createNewEvent(@RequestParam String eventName) {
-        events.add(eventName);
+   public String createNewEvent(@RequestParam String eventName, @RequestParam String eventDescription, @RequestParam String imgUrl) {
+        events.put(eventName, eventDescription);
+        url = imgUrl;
+
         return "redirect:/events"; // Instructs the browser to go to a different page
    }
 }
